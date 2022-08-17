73601e5581c88207e7cf564419ff0df11b6059b0
soumyadeep-git <dhalisoumyadeep@gmail.com>
2022-08-17 07:53:09 +0530

    Homework

---
+++ Simple_code/upperAndLower.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int input;
+        if (isupper(input))
+            cout << "Uppercase" << endl;
+        else
+            cout << "else" << endl;
+    }
+}
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void check(char ch)
+// {
+
+//     if (ch >= 'A' && ch <= 'Z')
+//         cout << ch << " is an UpperCase character\n";
+
+//     else if (ch >= 'a' && ch <= 'z')
+//         cout << ch << " is an LowerCase character\n";
+
+//     else
+//         cout << ch << " is not an aplhabetic character\n";
+// }
+
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void check(char ch)
+// {
+
+//     if (ch >= 'A' && ch <= 'Z')
+//         cout << ch << " is an UpperCase character\n";
+
+//     else if (ch >= 'a' && ch <= 'z')
+//         cout << ch << " is an LowerCase character\n";
+
+//     else
+//         cout << ch << " is not an aplhabetic character\n";
+// }
